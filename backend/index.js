ddbc29ff03e6e6c6e9c8d9500f6a075ab3903366
NishalNT <nishalnthingalaya1@gmail.com>
2023-11-23 19:43:33 +0530

    finally

--- backend/index.js
+++ backend/index.js
@@ -62,4 +62,16 @@ app.delete("/order/:id", async (req, res) => {
     }
 });
 
-module.exports = app;
+app.get("/order/:id", async (req, res) => {
+    try {
+        const order = await Order.findById({ _id: req.params.id });
+        res.send(order);
+    } catch (error) {
+        console.error("Error fetching order by ID:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost:${PORT}`);
+});
