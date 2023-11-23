e11741fa11125bce0dc11ca40f660eef186f93d9
NishalNT <nishalnthingalaya1@gmail.com>
2023-11-23 19:30:02 +0530

    new

--- backend/api/order.js
+++ backend/api/order.js
@@ -2,54 +2,54 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const Order = require('../models/Order');  
+const Order = require('../models/Order');
 
 const app = express();
 app.use(cors());
 
-app.post("/order", async (req, res) => {
+app.post("/api/order", async (req, res) => {
     try {
         const data = req.body;
         const createdOrder = new Order(data);
         await createdOrder.save();
         console.log("Order created:", createdOrder);
-        res.send("Order Created");
+        res.status(201).json({ message: "Order Created", order: createdOrder });
     } catch (error) {
         console.error("Error creating order:", error);
-        res.status(500).send({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
-app.get("/order", async (req, res) => {
+app.get("/api/order", async (req, res) => {
     try {
         const orderList = await Order.find();
-        res.send(orderList);
+        res.status(200).json(orderList);
     } catch (error) {
         console.error("Error fetching orders:", error);
-        res.status(500).send({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
-app.put("/order/:id", async (req, res) => {
+app.put("/api/order/:id", async (req, res) => {
     try {
         const data = req.body;
         await Order.updateOne({ _id: req.params.id }, { $set: data });
         console.log("Order Updated");
-        res.send("Order Updated");
+        res.status(200).send("Order Updated");
     } catch (error) {
         console.error("Error updating order:", error);
-        res.status(500).send({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
-app.delete("/order/:id", async (req, res) => {
+app.delete("/api/order/:id", async (req, res) => {
     try {
         await Order.deleteOne({ _id: req.params.id });
         console.log("Order Deleted");
-        res.send("Order Deleted");
+        res.status(200).send("Order Deleted");
     } catch (error) {
         console.error("Error deleting order:", error);
-        res.status(500).send({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
